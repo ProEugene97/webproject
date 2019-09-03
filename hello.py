41f1cdbbbfbd89f760ce92b81931c8fa03f031b8
ProEugene97 <proj9007@gmail.com>
2019-09-03 10:24:15 +0300

    gunicorn

---
+++ hello.py
@@ -0,0 +1,5 @@
+def app(environ, start_response):
+    start_response("200 OK", [
+    ("Content-Type", "text/plain"),]
+    )
+    return [bytes('\n'.join(environ['QUERY_STRING'].split('&')), encoding='utf8']
